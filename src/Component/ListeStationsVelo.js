25fe7e4e68f7a5f825002c2e9a607a2134e48d95
SebFox2011 <sebastien.philippe5@gmail.com>
2019-07-16 21:06:39 +0200

    Ajout de l'api Data Rennes Velo et recherche des stations

--- src/Component/ListeStationsVelo.js
+++ src/Component/ListeStationsVelo.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react';
 
 class ListeStationsVelo extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state={
+            list:[]
+        };
+    }
+
+
     //Appel à l'api
     componentDidMount() {
-        fetch('https://data.rennesmetropole.fr/api/datasets/1.0/search/')
+        fetch('https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=stations_vls')
             .then(response => response.json())
+            .then(data => this.setState({list: data["records"]}))
     }
 
     render() {
